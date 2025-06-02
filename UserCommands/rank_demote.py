c7b271d67cbcbb9a783efe2964c52b84446c316f
Aiden Smith <aidensmith@outlook.com>
2025-06-02 01:17:43 -0700

    fix app commands

--- UserCommands/rank_demote.py
+++ UserCommands/rank_demote.py
@@ -13,9 +13,9 @@ class RankDemote(commands.Cog):
         self.client = client
 
     @user_command(name='Rank | Demote', default_member_permissions=discord.Permissions(manage_roles=True), guild_ids=guilds)
-    async def demote_member(self, message, user):
-        if message.interaction.user.guild_permissions.manage_roles:
-            await message.defer(ephemeral=True)
+    async def demote_member(self, interaction: discord.Interaction, user: discord.Member):
+        if interaction.user.guild_permissions.manage_roles:
+            await interaction.defer(ephemeral=True)
             db = DB()
             db.connect()
             db.cursor.execute(f'SELECT rank, uuid FROM discord_links WHERE discord_id = {user.id}')
@@ -30,11 +30,11 @@ class RankDemote(commands.Cog):
                     embed = discord.Embed(title=':warning: Oops! We\'ve hit bedrock bottom.',
                                           description=f'Cannot demote any further.\nWhat so terrible could <@{user.id}> have done to deserve this? Since Trial-Starfish rank does not exist (yet), I suggest considering a termination from the guild.',
                                           color=0xebdb34)
-                    await message.respond(embed=embed, ephemeral=True)
+                    await interaction.respond(embed=embed, ephemeral=True)
                     db.close()
                     return
                 next_rank = discord_ranks[list(discord_ranks)[current_rank_index-1]]
-                all_roles = message.interaction.guild.roles
+                all_roles = interaction.guild.roles
                 roles_to_add = []
                 roles_to_remove = []
                 for add_role in next_rank['roles']:
@@ -42,7 +42,7 @@ class RankDemote(commands.Cog):
                     if role not in user.roles:
                         roles_to_add.append(role)
 
-                await user.add_roles(*roles_to_add, reason=f'Demotion (ran by {message.author.name})', atomic=True)
+                await user.add_roles(*roles_to_add, reason=f'Demotion (ran by {interaction.author.name})', atomic=True)
 
                 remove_roles = [x for x in discord_rank_roles if x not in next_rank['roles']]
                 for remove_role in remove_roles:
@@ -50,7 +50,7 @@ class RankDemote(commands.Cog):
                     if role in user.roles:
                         roles_to_remove.append(role)
 
-                await user.remove_roles(*roles_to_remove, reason=f'Demotion (ran by {message.author.name})', atomic=True)
+                await user.remove_roles(*roles_to_remove, reason=f'Demotion (ran by {interaction.author.name})', atomic=True)
 
                 try:
                     parts = user.nick.split(' ', 1)
@@ -66,16 +66,16 @@ class RankDemote(commands.Cog):
 
                 embed = discord.Embed(title=':white_check_mark: Demotion successful',
                                       description=f'<@{user.id}> demoted to **{list(discord_ranks)[current_rank_index-1]}**', color=0x3ed63e)
-                await message.respond(embed=embed)
+                await interaction.respond(embed=embed)
             else:
                 embed = discord.Embed(title=':no_entry: Oops! Something did not go as intended.',
                                       description=f'<@{user.id}> does not have a linked Minecraft account.\nPlease use the `/manage link` command first.',
                                       color=0xe33232)
-                await message.respond(embed=embed, ephemeral=True)
+                await interaction.respond(embed=embed, ephemeral=True)
                 db.close()
                 return
         else:
-            await message.interaction.response.send_message(
+            await interaction.interaction.response.send_message(
                 'You are missing Manage Roles permission(s) to run this command.', ephemeral=True)
 
     @commands.Cog.listener()
--- UserCommands/rank_promote.py
+++ UserCommands/rank_promote.py
@@ -13,9 +13,9 @@ class RankPromote(commands.Cog):
         self.client = client
 
     @user_command(name='Rank | Promote', default_member_permissions=discord.Permissions(manage_roles=True), guild_ids=guilds)
-    async def promote_member(self, message, user):
-        if message.interaction.user.guild_permissions.manage_roles:
-            await message.defer(ephemeral=True)
+    async def promote_member(self, interaction: discord.Interaction, user: discord.Member):
+        if interaction.user.guild_permissions.manage_roles:
+            await interaction.defer(ephemeral=True)
             db = DB()
             db.connect()
             db.cursor.execute(f'SELECT rank, uuid FROM discord_links WHERE discord_id = {user.id}')
@@ -30,11 +30,11 @@ class RankPromote(commands.Cog):
                     embed = discord.Embed(title=':warning: Oops!',
                                           description=f'Cannot promote past Narwhal.',
                                           color=0xebdb34)
-                    await message.respond(embed=embed, ephemeral=True)
+                    await interaction.respond(embed=embed, ephemeral=True)
                     db.close()
                     return
                 next_rank = discord_ranks[list(discord_ranks)[current_rank_index+1]]
-                all_roles = message.interaction.guild.roles
+                all_roles = interaction.guild.roles
                 roles_to_add = []
                 roles_to_remove = []
                 for add_role in next_rank['roles']:
@@ -42,7 +42,7 @@ class RankPromote(commands.Cog):
                     if role not in user.roles:
                         roles_to_add.append(role)
 
-                await user.add_roles(*roles_to_add, reason=f'Promotion (ran by {message.author.name})', atomic=True)
+                await user.add_roles(*roles_to_add, reason=f'Promotion (ran by {interaction.author.name})', atomic=True)
 
                 remove_roles = [x for x in discord_rank_roles if x not in next_rank['roles']]
                 for remove_role in remove_roles:
@@ -50,7 +50,7 @@ class RankPromote(commands.Cog):
                     if role in user.roles:
                         roles_to_remove.append(role)
 
-                await user.remove_roles(*roles_to_remove, reason=f'Promotion (ran by {message.author.name})', atomic=True)
+                await user.remove_roles(*roles_to_remove, reason=f'Promotion (ran by {interaction.author.name})', atomic=True)
 
                 try:
                     parts = user.nick.split(' ', 1)
@@ -66,16 +66,16 @@ class RankPromote(commands.Cog):
 
                 embed = discord.Embed(title=':white_check_mark: Promotion successful',
                                       description=f'<@{user.id}> promoted to **{list(discord_ranks)[current_rank_index+1]}**', color=0x3ed63e)
-                await message.respond(embed=embed)
+                await interaction.respond(embed=embed)
             else:
                 embed = discord.Embed(title=':no_entry: Oops! Something did not go as intended.',
                                       description=f'<@{user.id}> does not have a linked Minecraft account.\nPlease use the `/manage link` command first.',
                                       color=0xe33232)
-                await message.respond(embed=embed, ephemeral=True)
+                await interaction.respond(embed=embed, ephemeral=True)
                 db.close()
                 return
         else:
-            await message.interaction.response.send_message(
+            await interaction.response.send_message(
                 'You are missing Manage Roles permission(s) to run this command.', ephemeral=True)
 
     @commands.Cog.listener()
